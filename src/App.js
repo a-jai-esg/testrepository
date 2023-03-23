bbf987564ad0f7aea57504a85913ac1d28f11cbc
Elijah Nicholas Esguerra <ashesg1117@gmail.com>
2023-03-23 22:31:48 +0800

    First Commit - Test Project

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello, World.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>This is a react application.</h1>
       </header>
     </div>
   );
